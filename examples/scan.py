77276cfa80c13a9c130e399eefab1a99ab83741e
frazier-zh <11678751+frazier-zh@users.noreply.github.com>
2021-12-15 15:16:48 +0800

    updated output

--- examples/scan.py
+++ examples/scan.py
@@ -1,26 +1,19 @@
 import memboard as mb
-from memboard import wait, time, apply, measure, reset
+from memboard import wait, time, apply, measure, reset, output
 import memboard.unit as u
-
-import numpy as np
 
 def run():
     top_pin_list = [29, 30, 31, 32, 33]
     bottom_pin = 83
-    results = np.zeros((2, 5))
 
     reset('all')
     apply(pin=1, v=0.5 *u.V)
     apply(pin=84, v=0 *u.V)
 
     for i in range(5):
-        results[0, i] = time()
-        results[1, i] = measure(pin=bottom_pin, drive_pin=top_pin_list[i], v=0.1 *u.V)
+        output['time%d'%i] = time()
+        output['current%d'%i] = measure(pin=bottom_pin, drive_pin=top_pin_list[i], v=0.1 *u.V)
         wait(1 *u.us)
 
-    return results
-
-
 with mb.connect('../verilog/src/top.bit', debug=True):
-    mb.register(run)
-    mb.execute(every=20 *u.us, total=1 *u.s, out='scan')
+    mb.execute(run, every=20 *u.us, total=1 *u.s, filename='scan')
