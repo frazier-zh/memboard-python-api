04ab997cb4b1a692ca10bf4d17e63bd89d8438e5
frazier-zh <11678751+frazier-zh@users.noreply.github.com>
2021-12-15 15:45:54 +0800

    updated resistor.py

--- examples/resistor.py
+++ examples/resistor.py
@@ -1,16 +1,16 @@
 import memboard as mb
-from memboard import wait, time, apply, measure
+from memboard import wait, time, apply, measure, reset, output
 import memboard.unit as u
 
-import numpy as np
 
 def run():
     top_pin = 29
     bottom_pin = 84
 
-    apply(pin=1, v=2.5 *u.V)
+    reset('all')
+    output['time'] = time()
+    output['current'] = measure(pin=bottom_pin, drive_pin=top_pin, v=1 *u.V)
 
-    return time(), measure(pin=bottom_pin, drive_pin=top_pin, v=1 *u.V)
 
-with mb.connect('top.bit'):
-    mb.execute(run, every=200 *u.us, total=5 *u.s, out='test')
+with mb.connect('../verilog/src/top.bit'):
+    mb.execute(run, every=200 *u.us, total=5 *u.s, filename='test')
