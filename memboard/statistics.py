76b2f8e13376cf1df2e81a38106c6560edae0ae8
frazier-zh <11678751+frazier-zh@users.noreply.github.com>
2021-12-15 11:26:47 +0800

    updated values

--- memboard/statistics.py
+++ memboard/statistics.py
@@ -2,16 +2,20 @@ from . import unit as u
 import numpy as np
 
 _op_runtime = {
-    0x00:       0, # Do nothing
-    0x11:       0, # ADC reset
-    0x21:       1260, # ADC enable
-    0x12:       15, # DAC reset
-    0x22:       80, # DAC enable
-    0x13:       150, # Switch reset
-    0x23:       190, # Switch enable
-    0x04:       10, # Wait, 10ns
-    0x14:       0x1000000, # Wait, 0x1000000 * 10ns
-    0x06:       0, # Get FPGA time
+    0x00:       30, # Do nothing
+    0x11:       20, # ADC reset
+    0x21:       1390, # ADC enable
+    0x12:       50, # DAC reset
+    0x22:       60, # DAC enable
+    0x03:       10, # Wait, 10ns
+    0x13:       0x1000000, # Wait, 0x1000000 * 10ns
+    0x14:       190, # Switch1 reset
+    0x24:       190, # Switch1 enable
+    0x15:       190, # Switch2 reset
+    0x25:       190, # Switch2 enable
+    0x16:       190, # Switch3 reset
+    0x26:       190, # Switch3 enable
+    0x07:       30, # Get FPGA time
 }
 """Operation runtime (ns)
     code[7:0]   time
@@ -19,10 +23,10 @@ _op_runtime = {
 
 def get_runtime(ops):
     op_byte = ops%0x100
-    if op_byte in [0x04, 0x14]:
-        return _op_runtime[op_byte] * ops>>8 * 10 *u.ns
+    if op_byte in [0x03, 0x13]:
+        return (_op_runtime[op_byte]*(ops>>8)+_op_runtime[0]) *u.ns
     else:
-        return _op_runtime[op_byte] *u.ns
+        return (_op_runtime[op_byte]+_op_runtime[0]) *u.ns
 
 
 """Power domain"""
